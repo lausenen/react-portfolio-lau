91eae768438fef482e922cedbd716fa0e817d332
lausenen <lau.thomsen.privat@gmail.com>
2023-03-14 21:11:39 +0100

    Header

---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {BsLinkedin} from 'react-icons/bs'
+import {BsGithub} from 'react-icons/bs'
+const HeaderSocials = () => {
+  return (
+    <div className="header__socials">
+        <a href='https://www.linkedin.com/in/lau-thomsen-046b08260/' target="__blank"><BsLinkedin></BsLinkedin></a>
+        <a href='https://github.com/lausenen' target="__blank"><BsGithub></BsGithub></a>
+        <a href='https://linkedin.com' target="__blank"></a>
+    </div>
+    
+  )
+}
+
+export default HeaderSocials
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import './nav.css'
+import {AiOutlineHome} from 'react-icons/ai'
 const nav = () => {
   return (
-    <div>
-      Nav
-    </div>
+    <nav>
+      <a href='#'> <AiOutlineHome></AiOutlineHome></a>
+      <a href='#about'> <AiOutlineHome></AiOutlineHome></a>
+      <a href='#experience'> <AiOutlineHome></AiOutlineHome></a>
+      <a href='#services'> <AiOutlineHome></AiOutlineHome></a>
+      <a href='#contact'> <AiOutlineHome></AiOutlineHome></a>
+    </nav>
   )
 }
 
